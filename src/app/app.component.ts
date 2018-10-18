7a3c03647d3d5b311a5f4b173aa184ecf56e4847
Adam Morsi <adam.morsi@yahoo.com>
2018-10-18 16:02:05 +0300

    Creating animation with transition

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,37 @@
 import {Component} from '@angular/core';
+import {animate, state, style, transition, trigger} from '@angular/animations';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css']
+    styleUrls: ['./app.component.css'],
+    animations: [
+        trigger('divState', [
+            state('normal', style({
+                'background-color': 'red',
+                transform: 'translateX(0)'
+            })),
+            state('highlighted', style({
+                'background-color': 'blue',
+                transform: 'translateX(100px)'
+            })),
+            transition('normal <=> highlighted', animate(300))
+        ])
+    ]
 })
 export class AppComponent {
+    state = 'normal';
     list = ['Milk', 'Sugar', 'Bread'];
+
+    onAnimate() {
+        this.state === 'normal' ? this.state = 'highlighted' : this.state = 'normal';
+    }
 
     onAdd(item) {
         this.list.push(item);
+    }
+
+    onDelete(item) {
+        this.list.splice(this.list.indexOf(item), 1);
     }
 }

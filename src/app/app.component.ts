9826c0a15d6d59019df6a7014d34174f3e2d03ef
Adam Morsi <adam.morsi@yahoo.com>
2018-10-19 10:55:00 +0300

    End of section 26

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import {Component} from '@angular/core';
-import {animate, state, style, transition, trigger} from '@angular/animations';
+import {animate, group, keyframes, state, style, transition, trigger} from '@angular/animations';
 
 @Component({
     selector: 'app-root',
@@ -16,15 +16,106 @@ import {animate, state, style, transition, trigger} from '@angular/animations';
                 transform: 'translateX(100px)'
             })),
             transition('normal <=> highlighted', animate(300))
+        ]),
+        trigger('wildState', [
+            state('normal', style({
+                'background-color': 'red',
+                transform: 'translateX(0) scale(1)'
+            })),
+            state('highlighted', style({
+                'background-color': 'blue',
+                transform: 'translateX(100px) scale(1)'
+            })),
+            state('shrunken', style({
+                'background-color': 'green',
+                transform: 'translateX(0) scale(0.5)'
+            })),
+            transition('normal => highlighted', animate(300)),
+            transition('highlighted => normal', animate(800)),
+            transition('shrunken <=> *', [
+                style({
+                    'background-color': 'orange'
+                }),
+                animate(1000, style({
+                    'border-radius': '50px'
+                })),
+                animate(5000)
+            ])
+        ]),
+        trigger('list1', [
+            state('in', style({
+                opacity: 1,
+                transform: 'translateX(0)'
+            })),
+            transition('void => *', [
+                style({
+                    opacity: 0,
+                    transform: 'translateX(-100px)'
+                }),
+                animate(300)
+            ]),
+            transition('* => void', [
+                animate(300, style({
+                    opacity: 0,
+                    transform: 'translateX(100px)'
+                }))
+            ])
+        ]),
+        trigger('list2', [
+            state('in', style({
+                opacity: 1,
+                transform: 'translateX(0)'
+            })),
+            transition('void => *', [
+                animate(1000, keyframes([
+                    style({
+                        transform: 'translateX(-100px)',
+                        opacity: 0,
+                        offset: 0
+                    }),
+                    style({
+                        transform: 'translateX(-50px)',
+                        opacity: 0.5,
+                        offset: 0.3
+                    }),
+                    style({
+                        transform: 'translateX(-20px)',
+                        opacity: 1,
+                        offset: 0.8
+                    }),
+                    style({
+                        transform: 'translateX(0px)',
+                        opacity: 1,
+                        offset: 1
+                    })
+                ]))
+            ]),
+            transition('* => void', [
+                group([
+                    animate(300, style({
+                        color: 'red'
+                    })),
+                    animate(800, style({
+                        opacity: 0,
+                        transform: 'translateX(100px)'
+                    }))
+                ])
+            ])
         ])
     ]
 })
 export class AppComponent {
     state = 'normal';
+    wildState = 'normal';
     list = ['Milk', 'Sugar', 'Bread'];
 
     onAnimate() {
         this.state === 'normal' ? this.state = 'highlighted' : this.state = 'normal';
+        this.wildState === 'normal' ? this.wildState = 'highlighted' : this.wildState = 'normal';
+    }
+
+    onShrink() {
+        this.wildState = 'shrunken';
     }
 
     onAdd(item) {
@@ -33,5 +124,13 @@ export class AppComponent {
 
     onDelete(item) {
         this.list.splice(this.list.indexOf(item), 1);
+    }
+
+    animationStarted(event) {
+        console.log(event);
+    }
+
+    animationEnded(event) {
+        console.log(event);
     }
 }
